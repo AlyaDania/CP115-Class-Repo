c71c0257633ca93b2f2c0de7f1a7f576ec900d77
USERNAME <pellysherlynn@gmail.com>
2026-07-08 12:33:48 +0800

    finish lab03

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,10 @@
+kilobyte = float(input())
+megabyte = kilobyte * 1.024
+gigabyte = megabyte * 1.024
+terabyte = gigabyte * 1.024
+petabyte = terabyte * 1.024
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
+ 
