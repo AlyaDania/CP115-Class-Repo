31adc61df25aa8499d83cba348a86414c3f5b702
USERNAME <pellysherlynn@gmail.com>
2026-08-12 12:08:06 +0800

    finish lab tes2

---
+++ labs/lab-test2/lab-test-2.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")  #User input for name
+matric_no = input("Enter your matric number: ")
+
+print(f"Name: {name}\t\t\t\t\tMatric.NO: {matric_no}\n\n")
+print("*\t\t\t*")
+print("**\t\t**")
+print("***\t***")
+print("********")
+print("***\t***")
+print("**\t\t**")
+print("*\t\t\t*")
+print ("This is my\n\tsecond\n\t\tassignment")
+print("I want 2x10 marks, which is 20 full marks\n")
